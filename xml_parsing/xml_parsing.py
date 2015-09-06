a975750c16c617a7f37d6a027684517937a2c850
Davidslc <djjohnson@gmail.com>
2015-09-06 17:46:48 -0600

    Get list of tags

--- xml_parsing/xml_parsing.py
+++ xml_parsing/xml_parsing.py
@@ -5,5 +5,14 @@ def main():
 
     doc = xml.dom.minidom.parse("samplexml.xml")
 
+    print(doc.nodeName)
+    print(doc.firstChild.tagName)
+
+    # Get a list of XML tags from the document and print each one.
+    skills = doc.getElementsByTagName("skill")
+    print("%d skills:" % skills.length)
+    for skill in skills:
+        print(skill.getAttribute("name"))
+
 if __name__ == "__main__":
     main()
