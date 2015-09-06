88564ee84908ab28717bfc63735532a590d4f984
Davidslc <djjohnson@gmail.com>
2015-09-06 17:54:05 -0600

    Added code to create a new tag and add it into the doc

--- xml_parsing/xml_parsing.py
+++ xml_parsing/xml_parsing.py
@@ -14,5 +14,16 @@ def main():
     for skill in skills:
         print(skill.getAttribute("name"))
 
+    # Create a new XML tag and add it into the document.
+    new_skill = doc.createElement("skill")
+    new_skill.setAttribute("name", "jQuery")
+    doc.firstChild.appendChild(new_skill)
+    print("")
+
+    skills = doc.getElementsByTagName("skill")
+    print("%d skills:" % skills.length)
+    for skill in skills:
+        print(skill.getAttribute("name"))
+
 if __name__ == "__main__":
     main()
