8f0fb7caeee3b70a6352ef8bff3c7036cac35316
Davidslc <djjohnson@gmail.com>
2015-09-07 14:02:15 -0600

    Added comments to prime_generator.py.

--- prime_generator.py
+++ prime_generator.py
@@ -1,22 +1,21 @@
 # Helper function.
 def is_prime(n):
-    if n == 1:
+    if n == 1:  # One is never prime.
         return False
     for i in range(2, n):
         if n % i == 0:
-            return False
+            return False  # Found a divisor, not prime.
     else:
         return True
 
 
-# This is a generator function for finding prime numbers.
 def primes(number=1):
     while True:
         if is_prime(number):
-            yield number
+            yield number  # yield makes this a generator.
         number += 1
 
-for num in primes():
+for num in primes():  # Generate a list of prime numbers.
     if num > 100:
         break
     print(num)
